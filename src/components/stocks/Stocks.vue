19aac2a43606a2d6433d655480b1d66861aa4857
ajay sharma <ajaysh73@gmail.com>
2019-04-25 15:02:49 -0400

    Added Vues store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -9,15 +9,10 @@ export default {
   components: {
     appStock: Stock
   },
-  data () {
-    return {
-      stocks: [
-        { id: 1, name: 'AAPL', price: 1200 },
-        { id: 2, name: 'MSFT', price: 1000 },
-        { id: 3, name: 'DELL', price: 800 },
-        { id: 1, name: 'GOOGLE', price: 600 }
-      ]
-    };
+  computed: {
+    stocks () {
+      return this.$store.getters.stocks;
+    }
   }
 };
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: 'AAPL', price: 1200 },
+  { id: 2, name: 'MSFT', price: 1000 },
+  { id: 3, name: 'DELL', price: 800 },
+  { id: 1, name: 'GOOGLE', price: 600 }
+];
